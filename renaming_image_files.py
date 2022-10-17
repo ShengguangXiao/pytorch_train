0218c1c8fd86488b567d423b987bdbe4bdb62f91
Xiao Shengguang <shengguangxiao@gmail.com>
2022-10-17 13:46:38 +0800

    Add SPI wrong OCR images
    Add SPI wrong OCR images

---
+++ renaming_image_files.py
@@ -0,0 +1,19 @@
+import math, os
+import sys
+
+if __name__ == '__main__':
+    try:
+        [_, folder_path, start_index] = sys.argv
+    except:
+        print("Please input folder and start index")
+        exit
+
+    img_names = os.listdir(folder_path)
+    img_names = list(filter(lambda x: x.endswith('.png'), img_names))
+
+    index = int(start_index)
+    for i in range(len(img_names)):
+        img_name = img_names[i]
+        new_img_name = img_name[0] + "-" + str(index) + ".png"
+        os.rename(folder_path + img_name, folder_path + new_img_name)
+        index = index + 1
